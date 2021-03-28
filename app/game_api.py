6013703223d1ad9c62024068593256628efc4a33
Eric Liu <eliu390@gmail.com>
2021-03-28 00:10:08 +0000

    improved API

--- app/game_api.py
+++ app/game_api.py
@@ -12,6 +12,14 @@ def log_to_kafka(topic, event):
     producer.send(topic, json.dumps(event).encode())
 
 
+def valid_int(param):
+    try:
+        num = int(param)
+        return num >= 0
+    except:
+        return False
+    
+
 @app.route("/")
 def default_response():
     default_event = {'event_type': 'default'}
@@ -19,8 +27,32 @@ def default_response():
     return "This is the default response!\n"
 
 
-@app.route("/purchase_a_sword")
-def purchase_a_sword():
-    purchase_sword_event = {'event_type': 'purchase_sword'}
+@app.route("/purchase_sword")
+def purchase_sword():
+    buyer_id = request.args.get('buyer_id')
+    seller_id = request.args.get('seller_id')
+    
+    if !valid_int(buyer_id) or !valid_int(seller_id):
+        return "Invalid params!"
+    
+    purchase_sword_event = {'event_type': 'purchase_sword', 'buyer_id': buyer_id, 'seller_id': seller_id}
     log_to_kafka('events', purchase_sword_event)
-    return "Sword Purchased!\n"
+    return "Sword purchased from player " + seller_id + "!\n"
+
+
+@app.route("/join_guild")
+def join_guild():
+    player_id = request.args.get('player_id')
+    guild_id = request.args.get('guild_id')
+    join = request.args.get('join') # join=1 means joining guild, join=0 means leaving guild
+    
+    if !valid_int(player_id) or !valid_int(guild_id) or !valid_int(join):
+        return "Invalid params!"
+    
+    join_guild_event = {'event_type': 'join_guild', 'player_id': player_id, 'guild_id': guild_id, 'join': join}
+    log_to_kafka('events', join_guild_event)
+    if join == 1:
+        return "Joined guild " + guild_id + "!\n"
+    else:
+        return "Left guild " + guild_id + "!\n"
+
